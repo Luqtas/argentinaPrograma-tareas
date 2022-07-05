6479dbe290a5f41e9378ce53fce51ebbee2b2b53
Lucas <lucasdgonzalez@outlook.com>
2022-07-04 22:28:35 -0300

    tarea 2 clase 5
    Creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario. También vamos a crear un <h1> que diga Bienvenido!
    Vas a crear un botón de acción que una vez que lo apretás, va a mostrar toda la información junta en un campo de texto y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!

---
+++ clase-5/tarea-2/script.js
@@ -0,0 +1,19 @@
+const mostrarInfoUsuario = document.querySelector (".sendInfo");
+
+mostrarInfoUsuario.onclick = function (){
+
+    const userName = document.querySelector (".userName").value;
+    const userSecondName = document.querySelector (".userSecondName").value;
+    const userSurname = document.querySelector (".userSurname").value;
+    const userAge = Number (document.querySelector (".userAge").value);
+
+    if (userName === "" && userSecondName === "" && userSurname === "" && userAge === 0){
+        document.querySelector (".datosUser").innerText = "Por favor, completar los formularios."
+    }
+    else {
+        document.querySelector (".datosUser").innerText = `Su nombre es ${userName} ${userSecondName} ${userSurname}, y su edad es de ${userAge} años.`;
+        document.querySelector (".tittle").innerText = `Bienvenido ${userName}!`
+    }
+
+    return false;
+}
