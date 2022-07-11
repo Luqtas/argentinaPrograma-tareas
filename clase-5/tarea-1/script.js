8594281c206436ccd4df5e11cb2994e032b2e027
Lucas <lucasdgonzalez@outlook.com>
2022-07-11 15:05:27 -0300

    crea dos nuevos inputs de ingresos
    Agrega un input para calcular ingresos mensuales en base al ingreso anual, y otro input para calcular los ingresos diarios en base al sueldo por mes.

--- clase-5/tarea-1/script.js
+++ clase-5/tarea-1/script.js
@@ -1,19 +1,61 @@
-const calcularIngresos = document.querySelector (".calcularIngresos");
+const calcularIngresos = document.querySelector (".calcularIngresosPorMes");
+
+const MESES_DEL_ANIO = 12;
+const DIAS_DEL_MES = 30 - 8; // (Se restan los fines de semana)
 
 calcularIngresos.onclick = function(){
 
     let resultadoIngresos;
 
     let ingresosAnuales = Number (document.querySelector (".sueldoAnualUsuario").value);
-    const mesesDelAnio = 12;
 
-    resultadoIngresos = ingresosAnuales / mesesDelAnio;
+    resultadoIngresos = ingresosAnuales / MESES_DEL_ANIO;
  
     if (resultadoIngresos > 0){
         document.querySelector (".ingresoMensualUsuario").value = `Sueldo mensual de ${resultadoIngresos}`;
     }
     else{
         document.querySelector (".ingresoMensualUsuario").value = `Número inválido`;
+    };
+
+    return false;
+};
+
+const calcularIngresosPorAño = document.querySelector (".calcularIngresosPorAño");
+
+calcularIngresosPorAño.onclick = function (){
+
+    let resultadoIngresos;
+
+    const ingresosMensuales = Number (document.querySelector (".sueldoPorMes").value);
+
+    resultadoIngresos = ingresosMensuales * MESES_DEL_ANIO;
+
+    if (resultadoIngresos > 0){
+        document.querySelector (".IngresoAnual").value = `Sueldo anual de ${resultadoIngresos}`;
+    }
+    else{
+        document.querySelector (".IngresoAnual").value = `Número inválido`;
+    };
+
+    return false;
+};
+
+const calcularIngresosDiarios = document.querySelector (".calcularIngresosPorDia");
+
+calcularIngresosDiarios.onclick = function (){
+
+    let resultadoIngresos;
+
+    const ingresosMensuales = Number (document.querySelector (".sueldoPorMesDos").value);
+
+    resultadoIngresos = ingresosMensuales / DIAS_DEL_MES;
+
+    if (resultadoIngresos > 0){
+        document.querySelector (".IngresoDiario").value = `Sueldo diario de ${resultadoIngresos}`;
+    }
+    else{
+        document.querySelector (".IngresoDiario").value = `Número inválido`;
     };
 
     return false;
